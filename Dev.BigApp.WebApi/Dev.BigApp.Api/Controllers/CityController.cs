ca7c51b0e3b19fdca209e050acb1d36d5d1ebf53
4hmetuyar <4hmetuyar@gmail.com>
2018-10-16 11:58:59 +0300

    City controller added.

---
+++ Dev.BigApp.WebApi/Dev.BigApp.Api/Controllers/CityController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Dev.BigApp.Api.Controllers
+{
+    public class CityController : Controller
+    {
+        /// <summary>
+        /// Get city list metod.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Get city by city id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
